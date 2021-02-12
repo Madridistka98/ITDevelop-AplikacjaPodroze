96e984ab3076f0e130245edda12986817d41ec00
Madridistka98 <marchewicz.karolina9@gmail.com>
2021-02-12 18:15:08 +0100

    Added navigation

--- podroze/resources/views/auth/login.blade.php
+++ podroze/resources/views/auth/login.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <div class="container">
-    <div id="example"></div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
--- podroze/resources/views/layouts/app.blade.php
+++ podroze/resources/views/layouts/app.blade.php
@@ -20,11 +20,11 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }} 
+    <div id="app" class="d-flex flex-column">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+            <div class="container-fluid">
+                <a class="navbar-brand col-7 col-sm-6 col-md-2" href="{{ url('/') }}">
+                    <img class="img img-fluid" src="./static/images/Nazwa_dinozaur.png" alt="logo_nawigacja"> 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -32,8 +32,16 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
+                    <ul class="navbar-nav mr-auto ">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link text-center text-white mb-2 mb-md-0 pb-0 pl-0 h5">{{ __('Tickets') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link text-center text-white mb-2 mb-md-0 pb-0 h5">{{ __('Hotels') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link text-center text-white mb-2 mb-md-0 pb-0 h5">{{ __('About Us') }}</a>
+                            </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -42,13 +50,7 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link btn btn-success rounded-pill font-weight-bolder px-5 py-1" href="{{ route('login') }}">{{ __('Sign In') }}</a>
                                 </li>
                             @endif
                         @else
@@ -80,6 +82,16 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <footer class="bg-dark container-fluid align-self-end">
+            <div class="row">
+                <div class="col-2">
+                <a class="col-7 col-sm-6 col-md-2" href="{{ url('/') }}">
+                    <img class="img img-fluid" src="./static/images/Nazwa_dinozaur.png" alt="logo_nawigacja"> 
+                </a>
+                </div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
