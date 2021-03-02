776098af6922fe2c2af3fb2f07760d2f6c069b63
Volodyyr Zhydkykh <zhydkykhv@gmail.com>
2021-03-02 14:34:42 +0100

    backend google maps

--- podroze/app/Http/Controllers/DestinationController.php
+++ podroze/app/Http/Controllers/DestinationController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destination;
 use Illuminate\Http\Request;
 
 class DestinationController extends Controller
 {
-    public function getDestination(){
+    public function index()
+    {
         return Destination::all();
     }
 }
