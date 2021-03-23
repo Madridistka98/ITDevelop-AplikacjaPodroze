52282de669f35ddc03bba5704f6957a440a38956
Madridistka98 <marchewicz.karolina9@gmail.com>
2021-03-23 09:10:24 +0100

    transportation saving

--- podroze/resources/js/components/Calendar/Trip.jsx
+++ podroze/resources/js/components/Calendar/Trip.jsx
@@ -54,7 +54,31 @@ function Trip(props: Props): Node {
     );
     console.log(trip);
     if (trip.hasOwnProperty("id")) {
-        const destinations = trip.destinations.map((dest) => {});
+        const destinationsComponents = [];
+        for (let index = 0; index < trip.destinations.length; index++) {
+            let destinationsComponent = (
+                <div
+                    className="bg-info trip__point d-flex flex-row flex-grow-1 mx-5 my-2"
+                    key={index}
+                >
+                    <img
+                        src="./static/images/destination-1.jpg"
+                        alt="destination"
+                        className="img img-fluid w-50 h-75 my-auto"
+                    />
+                    <p className="flex-grow-1 h3 text-center my-auto">
+                        {trip.destinations[index].city +
+                            " " +
+                            trip.destinations[index].country}
+                    </p>
+                </div>
+            );
+            destinationsComponents.push(destinationsComponent);
+        }
+        let destinationsNames = trip.destinations.map((dest) => {
+            return dest.city + ", " + dest.country;
+        });
+        destinationsNames = destinationsNames.join("--");
         result = (
             <div className="row bg-dark rounded p-4">
                 <div className="bg-secondary col-md-4 col-12 position-relative d-flex flex-column p-5">
@@ -63,7 +87,7 @@ function Trip(props: Props): Node {
                     </p>
                     <h3 className="calendar__trip-name text-center h2 my-auto">
                         <a
-                            href={`/searchtrip?start=${trip.start.city}, ${trip.start.country}&destination=${trip.destination.city}, ${trip.destination.country}=&date=${apiDate}&day=23&month=3&year=2021`}
+                            href={`/searchtrip?start=${trip.start.city}, ${trip.start.country}&destination=${trip.destination.city}, ${trip.destination.country}&date=${apiDate}&destinations=${destinationsNames}&transport=${trip.transport}`}
                         >
                             {trip.name}
                         </a>
@@ -96,26 +120,7 @@ function Trip(props: Props): Node {
                     </div>
 
                     <div className="d-flex flex-column">
-                        <div className="bg-info trip__point d-flex flex-row flex-grow-1 mx-5 my-2">
-                            <img
-                                src="./static/images/destination-1.jpg"
-                                alt="destination"
-                                className="img img-fluid w-50 h-75 my-auto"
-                            />
-                            <p className="flex-grow-1 h3 text-center my-auto">
-                                PURO Wrocław Stare Miasto
-                            </p>
-                        </div>
-                        <div className="bg-info trip__point d-flex flex-row flex-grow-1 mx-5 my-2">
-                            <img
-                                src="./static/images/destination-1.jpg"
-                                alt="destination"
-                                className="img img-fluid w-50 h-75 my-auto"
-                            />
-                            <p className="flex-grow-1 h3 text-center my-auto">
-                                PURO Wrocław Stare Miasto
-                            </p>
-                        </div>
+                        {destinationsComponents}
                     </div>
                 </div>
             </div>
